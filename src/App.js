fd07bb195b977da42c04ce2f6ffc8636ad4c2eba
Shahar Levi <shaharLe88@gmail.com>
2023-01-22 15:25:16 +0200

    Adding useState, useEffect to Groceries List & Shopping Cart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+import React, { useState, useEffect } from 'react'
 import { GroceriesList } from './Components/GroceriesList'
 import { ShoppingCart } from './Components/ShoppingCart'
 import { groceries,shoppingCart } from './Local Data/data'
 import './App.css';
 
 function App() {
+  const [groceriesItems, setGroceriesItems] = useState([])
+  const [shoppingCartItems, setShoppingCartItems] = useState([])
+  // initilaize Grocories List & shoppingCart with useEffect Hook
+  useEffect(() => {
+    setGroceriesItems(groceries)
+    setShoppingCartItems(shoppingCart)
+  }, [], [])
+
+
   return (
     <div className="App">
 
       <div className="container-fluid mt-2">
         <div className="row">
           <div className="col-md-6 col-sm-12 alert alert-primary">
-            <GroceriesList header="Groceries List" data={groceries} />
+            <GroceriesList header="Groceries List" data={groceriesItems} />
           </div>    
           <div className="col-md-6 col-sm-12 alert alert-warning">
-            <ShoppingCart header="Shopping Cart" data={shoppingCart} />
+            <ShoppingCart header="Shopping Cart" data={shoppingCartItems} />
           </div>
         </div>
       </div>
