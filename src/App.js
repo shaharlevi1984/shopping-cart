d028d2e034e7aae7650dbf9864cf486b0d4611bb
Shahar Levi <shaharLe88@gmail.com>
2023-01-22 15:10:39 +0200

    Add Groceries List Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import {GroceriesList} from './Components/GroceriesList'
+import {groceries} from './Local Data/data'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="container-fluid mt-2">
+      <div className="col-md-6 col-sm-12 alert alert-primary">
+            <GroceriesList header="Groceries List" data={groceries}  />
+          </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/GroceriesList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export function GroceriesList(props) {
+    return (
+      <ul className="list-group">
+          <li className="list-group-item active" aria-current="true" >{props.header} </li>
+          {
+              props.data.map(item => (
+                  <li key={item.id} className="list-group-item d-flex justify-content-between align-center mt-4" 
+                  style={{cursor: item.quantity ? 'pointer' : 'not-allowed'}}>
+                  {item.name} - Stock: {item.quantity} - price: {item.price}
+                  <button type="button" id={item.id} className= {item.quantity ? 'btn btn-primary' : 'btn btn-info' }
+                  //onClick={() => props.itemClicked(item)}
+                  disabled= {item.quantity ? '' : 'disables'}> Add to Cart </button>
+                  </li>)
+              )
+          }
+      </ul>
+    )
+}
